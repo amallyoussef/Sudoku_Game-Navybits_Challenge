2e6229d935ea08d0821bafa68b0bf8376901eb77
amallyoussef <amalyoussef2000@hotmail.com>
2024-11-14 14:59:50 +0200

    Target 1: Sudoku Grid Initialization

---
+++ src/Components/SudokuBoard.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const SudokuBoard = () => {
+  //Initialize a 9x9 empty grid
+  const [board, setBoard] = useState<string[][]>(
+    Array.from({ length: 9 }, () => Array(9).fill(""))
+  );
+
+  //Handler Function for Input Change
+  const handleInputChange = (row: number, col: number, value: string) => {
+    //Validate input: only numbers between 1-9 or empty strings
+    if (/^[1-9]?$/.test(value)) {
+      const newBoard = [...board];
+      newBoard[row][col] = value;
+      setBoard(newBoard);
+    }
+  };
+
+  return (
+    <div className="sudoku-board">
+      {board.map((row, rowIndex) => (
+        <div key={rowIndex} className="sudoku_row">
+          {row.map((cell, colIndex) => (
+            <input
+              key={colIndex}
+              className="sudoku-cell"
+              type="text"
+              maxLength={1}
+              value={cell}
+              onChange={(num) =>
+                handleInputChange(rowIndex, colIndex, num.target.value)
+              }
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SudokuBoard;
